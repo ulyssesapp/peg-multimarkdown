131c9bcdbb4f6e2a52b3e575e70169cf5cfa2911
Friedrich Gräter <friedrich@the-soulmen.com>
2012-02-24 13:56:10 +0100

    Removing dead code

--- markdown_lib.c
+++ markdown_lib.c
@@ -142,43 +142,6 @@ static element * process_raw_blocks(element *input, int extensions, element *ref
     return input;
 }
 
-/* markdown_to_gstring - convert markdown text to the output format specified.
- * Returns a GString, which must be freed after use using g_string_free(). */
-GString * markdown_to_g_string_legacy(const char *text, int extensions, int output_format) {
-    element *result;
-    element *references;
-    element *notes;
-    element *labels;
-    GString *formatted_text;
-    GString *out;
-    out = g_string_new("");
-
-    formatted_text = preformat_text(text);
-
-    if (output_format == OPML_FORMAT) {
-        result = parse_markdown_for_opml(formatted_text->str, extensions);
-    } else {
-        references = parse_references(formatted_text->str, extensions);
-        notes = parse_notes(formatted_text->str, extensions, references);
-        labels = parse_labels(formatted_text->str, extensions, references, notes);
-        result = parse_markdown_with_metadata(formatted_text->str, extensions, references, notes, labels);
-
-        result = process_raw_blocks(result, extensions, references, notes, labels);
-    }
-
-    g_string_free(formatted_text, TRUE);
-
-    print_element_list(out, result, output_format, extensions);
-
-    free_element_list(result);
-
-    if (output_format != OPML_FORMAT) {
-        free_element_list(references);
-        free_element_list(labels);
-    }
-    return out;
-}
-
 /* markdown_to_opml_frontend - convert markdown text to OPML
  */
 GString * markdown_to_g_string_opml_frontend(const char *text, int extensions) {
