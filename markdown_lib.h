3bed070aa322a5e35da92978d7316f2cff829756
Friedrich Gräter <friedrich@the-soulmen.com>
2013-03-07 16:29:32 +0100

    Remove extension request for storing tags

--- markdown_lib.h
+++ markdown_lib.h
@@ -12,8 +12,7 @@ enum markdown_extensions {
     EXT_FILTER_STYLES    = 1 << 3,
     EXT_COMPATIBILITY    = 1 << 4,
     EXT_PROCESS_HTML     = 1 << 5,
-	EXT_NO_LABELS		 = 1 << 6,
-	EXT_KEEP_TAGS		 = 1 << 7
+	EXT_NO_LABELS		 = 1 << 6
 };
 
 enum markdown_formats {
